8e929162b8704f73f4589a4e2cc076c8edbc69bd
khanggg302222 <khannnng302@gmail.com>
2025-10-15 21:34:57 +0700

    Update model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+# This is the User model definition file used for FastAPI and MongoDB.
+from pydantic import BaseModel
+class User(BaseModel):
+    name: str
+    email: str
+    age: int
+
+
+class Userresponse(User):
+    id: str
+
